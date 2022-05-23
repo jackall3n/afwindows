f1fc4c14271b1ca1595e872271d32f2e1655ebc1
Jack Allen <jackallensemail+git@gmail.com>
2022-05-23 18:19:31 +0100

    script

--- tools/ignore-build.sh
+++ tools/ignore-build.sh
@@ -1,6 +1,6 @@
 
 # Name of the app to check. Change this to your application name!
-APP=client
+APP=$PROJECT_NAME
 
 # Determine version of Nx installed
 NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
